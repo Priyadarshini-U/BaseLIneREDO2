b1e1cdcfaa6bd915476be679f50c6952c1d3cf6a
priyadarshini <priyadr@thoughtworks.com>
2015-09-04 16:27:43 +0530

    Zero Sales Tax for good of type food

--- src/com/tw/shopping/Good.java
+++ src/com/tw/shopping/Good.java
@@ -1,17 +1,19 @@
 package com.tw.shopping;
 
 public class Good {
-
     private String name;
     private double price;
+    private String type;
 
-    public Good(String name, double price) {
+    public Good(String name, double price, String type) {
         this.name = name;
         this.price = price;
+        this.type = type;
     }
 
     public double salesTax() {
+        if(type.equals("food"))
+            return 0.0;
         return price / 10;
     }
-
 }
--- test/com/tw/shopping/GoodTest.java
+++ test/com/tw/shopping/GoodTest.java
@@ -8,7 +8,11 @@ public class GoodTest {
 
     @Test
     public void shouldHaveSalesTaxForGood() {
-        assertEquals(new Good("cds", 34.90).salesTax(), 3.49, 0.1);
+        assertEquals(new Good("cds", 34.90, "accessories").salesTax(), 3.49, 0.1);
     }
 
+    @Test
+    public void shouldHaveZeroSalesTaxForGoodOfTypeFood() {
+        assertEquals(new Good("rice", 34.90, "food").salesTax(), 0.0, 0.1);
+    }
 }
